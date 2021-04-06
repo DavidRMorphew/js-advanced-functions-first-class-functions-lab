7029c63340b8e8788b17008dfbbc1a22c4242460
DavidRMorphew <morphewd@umich.edu>
2021-04-05 21:58:42 -0400

    commit tree 0276c62b522d499c90c4b345af2bfdb3ebb22bbd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => { return array.slice(0,2); };
+
+const returnLastTwoDrivers = (array) => { return array.slice(-2); };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// why not arrow function?
+function createFareMultiplier(integer){ 
+    return function(multiplier){ 
+        return integer * multiplier }
+}
+
+function fareDoubler(fare){
+    const fareDoubled = createFareMultiplier(fare)(2)
+    return fareDoubled
+}
+
+function fareTripler(fare){
+    const fareTripled = createFareMultiplier(fare)(3)
+    return fareTripled
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driversFunction){
+    return driversFunction(arrayOfDrivers);
+}
